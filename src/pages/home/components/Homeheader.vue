f6fbf063bdf3ff279bd7f7f51a71924d23ee7ec8
Your Name <email@example.com>
2019-03-12 16:31:06 +0800

    liuxin

---
+++ src/pages/home/components/Homeheader.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="header">
+		<div class="header-left">
+			<span class="iconfont">&#xe624;</span>
+		</div>
+		<div class="header-mind">
+			<span class="iconfont">&#xe632;</span>
+			输入城市/景点/游玩主题
+		</div>
+		<div class="header-right">
+			城市
+			<span class="iconfont city-icon">&#xe6aa;</span>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'HomeHeader'
+	}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.header
+		height:.86rem
+		line-height:.86rem
+		background:$bgColor
+		display:flex
+		color:#fff
+		.header-left
+			width:.62rem
+			float:left
+			text-align:center
+		.header-mind
+			flex:1
+			margin-top:.12rem
+			margin-left:.2rem
+			line-height:.62rem
+			height:.62rem
+			background-color:#fff
+			border-radius:.1rem
+			color:#999
+			padding-left:.2rem
+		.header-right
+			width:1.24rem
+			float:right
+			text-align:center
+			.city-icon
+				margin-left:-.04rem
+				font-size:.3rem
+</style>
